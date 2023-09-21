fa8570a72788dbaea3f5d18c102cde7da5e8ad72
KK <kk@KK-Macbook.local>
2023-09-21 18:53:09 +0800

    add distributed

--- lgit.cpp
+++ lgit.cpp
@@ -4,5 +4,5 @@ using namespace std;
 int main()
 {
     cout<<"hello world"<<endl;
-    cout<<"git is a version control system"<<endl;
+    cout<<"git is a distributed version control system"<<endl;
 }
