4c6faec6aa7fdb4b410bc858546efa6fe681157e
KK <kk@KK-Macbook.local>
2023-10-11 22:57:55 +0800

    delete version

--- lgit.cpp
+++ lgit.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main()
 {
     cout<<"hello world"<<endl;
-    cout<<"git is a distributed version control system"<<endl;
+    cout<<"git is a distributed control system"<<endl;
 
 }
